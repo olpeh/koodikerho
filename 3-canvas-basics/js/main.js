95004f1b28f1c039e57919b09ee010ee42d8a5f4
Olavi Haapala <ojhaapala@gmail.com>
2017-11-16 19:47:23 +0200

    Start implementing basic structure for snake

--- 3-canvas-basics/js/main.js
+++ 3-canvas-basics/js/main.js
@@ -1,1 +1,29 @@
 console.log('Hello world');
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'blue';
+
+var posX = 295;
+var posY = 295;
+var sizeX = 10;
+var sizeY = 10;
+
+var i = 0;
+function timeout() {
+  setTimeout(function() {
+    if (i < 295) {
+      console.log('Looppi: ' + i++);
+      ctx.clearRect(0, 0, 600, 600);
+      ctx.fillRect(posX, posY - i, sizeX, sizeY);
+      timeout();
+    } else {
+      //  i = 0;
+      //   timeout();
+      alert('Game over');
+    }
+  }, 10);
+}
+
+timeout();
